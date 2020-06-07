6fe52732e6f3101a458b8b3d2842b1da09f68490
Denis Bar <piccula@mail.ru>
2020-06-07 23:12:05 +0300

    five step

---
+++ src/even.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+  console.log('Welcome to the Brain-Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
+  const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  const isEven = (num) => num % 2 === 0;
+
+  let count = 0;
+  while (count < 3) {
+    const number = getRandomInRange(0, 20);
+    console.log(`Question: ${number}`);
+    const UserAnswer = (readlineSync.question('Answer: '));
+    const result = isEven(number) ? 'yes' : 'no';
+    if (UserAnswer === result) {
+      console.log('Correct!');
+      count += 1;
+    } else {
+      console.log('Uncorrect! Try again.');
+      if (count > 0) {
+        count -= 1;
+      } else {
+        count = 0;
+      }
+    }
+  }
+  console.log(`Congratulation, ${name}!`);
+};
